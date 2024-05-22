46220fbe1b48481a124e6b2cbc4a454c683739dd
webshakil <chumkysk@gmail.com>
2024-05-22 17:06:51 +0600

    some modification

--- index.js
+++ index.js
@@ -1,57 +1,56 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
-import cors from 'cors'
-
-
-
-
+import cors from 'cors';
 import db from "./mongoC.js";
 
-const port = 4000;
-const app = express();
 dotenv.config();
+
+const port = process.env.PORT || 4000;
+const app = express();
+
 const corsOptions = {
-    origin: 'http://3.83.248.93:3000', // Replace with the allowed origin
-    optionsSuccessStatus: 200 // Some legacy browsers choke on 204
-  };
-  
-  app.use(cors(corsOptions));
-app.use((_req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-  
-    next();
-  });
+  origin: 'http://3.83.248.93:3000', // Replace with the allowed origin
+  optionsSuccessStatus: 200 // Some legacy browsers choke on 204
+};
+
+app.use(cors(corsOptions));
 
 // Parses the text as url encoded data
 app.use(bodyParser.urlencoded({ extended: true }));
- 
+
 // Parses the text as json
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World, from express shakil');
-})
+  res.send('Hello World, from express shakil');
+});
 
-app.post('/addUser',async (req, res) => {
-    let collection = await db.collection("users");
-    let newDocument = req.body;
+app.post('/addUser', async (req, res) => {
+  try {
+    const collection = await db.collection("users");
+    const newDocument = req.body;
     newDocument.date = new Date();
-    let result = await collection.insertOne(newDocument);
-    console.log("rreq"+req.body);
-    res.send(result).status(204);
+    const result = await collection.insertOne(newDocument);
+    console.log("Request body:", req.body);
+    res.status(201).send(result); // 201 Created
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-app.get('/getUsers', async(req, res) => {
-    let collection = await db.collection("users");
-    let results = await collection.find({})
-      
-      .toArray();
-    res.send(results).status(200);
+app.get('/getUsers', async (req, res) => {
+  try {
+    const collection = await db.collection("users");
+    const results = await collection.find({}).toArray();
+    res.status(200).send(results); // 200 OK
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-app.listen(port, function () {
-    console.log("Server is listening at port:" + port);
+app.listen(port, () => {
+  console.log(`Server is listening at port: ${port}`);
 });
- 
